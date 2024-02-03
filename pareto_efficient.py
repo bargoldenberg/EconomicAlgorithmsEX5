223457f15f9b016ad4287322288f93266f062ad0
Bar Goldenberg <barg991@gmail.com>
2024-02-03 19:58:55 +0200

    fixed code

--- pareto_efficient.py
+++ pareto_efficient.py
@@ -1,8 +1,7 @@
 import networkx as nx
 import math
 
-def is_pareto_efficient(valuations: list[list[float]], allocation: 
-list[list[float]] ) -> bool:
+def is_pareto_efficient(valuations: list[list[float]], allocation: list[list[float]] ) -> bool:
     G = build_graph(valuations, allocation)
     try:
         nx.find_negative_cycle(G,0, weight="weight") #Throws error if there is no negative cycle.
@@ -10,26 +9,23 @@ list[list[float]] ) -> bool:
     except:
         return True
 
-def build_graph(valuations: list[list[float]], allocation: 
-list[list[float]] ) -> nx.DiGraph:
+def build_graph(valuations: list[list[float]], allocation: list[list[float]] ) -> nx.DiGraph: #Builds Clique
     G = nx.DiGraph()
     player_num = len(valuations)
     G.add_nodes_from(range(player_num))
     for i in range(player_num):
         for j in range(player_num):
             if i != j:
-                selected_weight =(math.log10(get_min_ratio(i, j, 
-allocation)))
+                selected_weight =(math.log10(get_min_ratio(i, j, valuations, allocation)))
                 G.add_edge(i, j, weight=selected_weight)
     return G
 
-def get_min_ratio(i, j, allocation):
-    i_allocations = allocation[i]
+def get_min_ratio(i, j, valuations, allocation):
     min_val = float('inf')
-    for k in range(len(i_allocations)):
-        if i_allocations[k] == 0 or allocation[j][k] == 0:
+    for k in range(len(allocation[i])):
+        if allocation[i][k] == 0 or allocation[j][k] == 0:
             continue
-        curr_val = allocation[i][k] / allocation[j][k]
+        curr_val = (valuations[i][k] * allocation[i][k]) / (valuations[j][k] * allocation[j][k])
         if curr_val < min_val:
             min_val = curr_val
     return min_val
